16b851095e385e95c716a3c2980ff0e4e017a110
ces0491 <cesairetobias@hotmail.com>
2020-09-14 13:05:20 +0200

    bug with XLConnect version 1.0.1 and loading locked workbooks created in Excel 2007.

--- R/unlock_wb.R
+++ R/unlock_wb.R
@@ -2,17 +2,20 @@
 #'
 #' @param file_dir \code{data.frame} of file paths and names for files you wish to unlock
 #' @param options.java.params optional string argument specifying java parameters
+#' @param wb_password optional string argument. use for testing. not advised to store passwords in script
 #'
 #' @return NULL
 #' @export
 #'
-unlock_wb <- function(file_dir, options.java.params = NULL) {
+unlock_wb <- function(file_dir, options.java.params = NULL, wb_password = NULL) {
 
   options(java.parameters = options.java.params)
 
   assertR::assert_present(names(file_dir), c("file_path", "file_name"))
 
-  wb_password <- rstudioapi::askForPassword("Please enter password to unlock workbook") # enter password via rstudio api rather than a string for security
+  if(is.null(wb_password)) {
+    wb_password <- rstudioapi::askForPassword("Please enter password to unlock workbook") # enter password via rstudio api rather than a string for security
+  }
 
   # you may unlock multiple files in a directory provided they all have the same password
   for(n in 1:nrow(file_dir)) {
@@ -22,12 +25,12 @@ unlock_wb <- function(file_dir, options.java.params = NULL) {
     file.exists <- base::file.exists(reqd_file$file_path)
     assertR::assert_true(file.exists, "the file you're trying to unlock doesn't exist")
 
+    wb <- XLConnect::loadWorkbook(reqd_file$file_path, password = wb_password)
+    XLConnect::saveWorkbook(wb)
+
     p <- which(file_dir$file_path == reqd_file$file_path)
     progress <- round(p/length(file_dir$file_path), 2) * 100
     print(glue::glue("{reqd_file$file_name} unlocked - {progress}% complete"))
-
-    wb <- XLConnect::loadWorkbook(reqd_file$file_path, password = wb_password)
-    XLConnect::saveWorkbook(wb)
 
     rm(wb)
     XLConnect::xlcFreeMemory()  # free Java Virtual Machine memory
---
+++ tests/testthat/test-unlock_wb.R
@@ -0,0 +1,14 @@
+test_that("unlock wb works", {
+
+  # test_file <- "locked_wb_test.xlsx"
+  # password <- "123"
+  # src_dir <- system.file("extdata", package = "excelsioR")
+  # file_dir_df <- data.frame(file_path = src_dir, file_name = test_file)
+  #
+  # test_wb <- unlock_wb(file_dir_df, wb_password = password)
+  #
+  # test_df <- read_wb(src_dir)
+  # expected_df <- data.frame(test, wb, unlocked)
+  #
+  # expect_equal(test_df, expected_df)
+})
